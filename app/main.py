0cc5e31374394793910c739dd0eb0c14395e1f96
Amaru_0 <bogdanbilas38@gmail.com>
2026-08-02 14:11:53 +0100

    1

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
-from . import models
+from . import models, books, authors
 from .database import engine
-from .routers import books
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Book API")
 
 app.include_router(books.router)
-
+app.include_router(authors.router)
 
 @app.get("/")
 def root():
